e26226ceb9f86f08664ec9ef9c71617f3ce6f4b9
TabzKid <kidane.tabo@gmail.com>
2019-02-18 21:37:58 +0100

    Implementation de méthode (StringCalculator) pour test "testWith1and2"

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
     public StringCalculator() {
     }
 
     public int add(String numbers) {
         int total = 0;
-        int sum = 0;
+        List<String> tabNumbers = new ArrayList<>();
         if(!numbers.equals("")) {
-            sum = Integer.parseInt(numbers);
+            tabNumbers = Arrays.asList(numbers.split(","));
+
+            for (String strNumber : tabNumbers) {
+                total += Integer.parseInt(strNumber);
+            }
         }
 
-        return total + sum;
+        return total;
     }
 }
